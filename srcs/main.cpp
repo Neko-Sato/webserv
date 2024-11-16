92ad6d38675e62f10e1df4ad7b39eefea7600602
Neko-Sato <hisatosi35350@gmail.com>
2024-11-17 03:22:32 +0900

    update

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,78 +1,41 @@
-#include <EventLoop/BaseIOWatcher.hpp>
-#include <EventLoop/BaseSignalWatcher.hpp>
-#include <EventLoop/BaseTimerWatcher.hpp>
-#include <EventLoop/BaseProcessWatcher.hpp>
 #include <EventLoop.hpp>
+#include <Watchers/IOWatcher.hpp>
+#include <Watchers/ProcessWatcher.hpp>
+#include <Watchers/SignalWatcher.hpp>
+#include <Watchers/TimerWatcher.hpp>
 #include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
 
-class MyTimerWatcher : public ftev::EventLoop::BaseTimerWatcher {
-private:
-  int _count;
+void on_timeout(ftev::TimerWatcher<int> &watcher, int _) {
+  (void)_;
+  std::cout << "Timer fired!" << std::endl;
+  watcher.start(1000);
+}
 
-public:
-  MyTimerWatcher(ftev::EventLoop &loop)
-      : ftev::EventLoop::BaseTimerWatcher(loop), _count(0) {
+void on_read(ftev::IOWatcher<int> &watcher, int _) {
+  (void)_;
+  char buf[10];
+  ssize_t size = read(watcher.get_fd(), buf, 10);
+  if (size == 0) {
+    watcher.close();
+    watcher.loop.stop();
+    return;
   }
-  ~MyTimerWatcher() {
-  }
-  void on_timeout() {
-    _count++;
-    std::cout << "Timer fired!" << std::endl;
-    // if (_count == 5)
-    //   _loop.stop();
-    // else
-    start(100);
-  }
-};
+  size = write(1, buf, size);
+}
 
-class MyIOWatcher : public ftev::EventLoop::BaseIOWatcher {
-public:
-  MyIOWatcher(ftev::EventLoop &loop, char const *filename)
-      : ftev::EventLoop::BaseIOWatcher(loop, open(filename, O_RDONLY)) {
-  }
-  ~MyIOWatcher() {
-  }
-  void on_read() {
-    char buf[10];
-    ssize_t size = read(get_fd(), buf, 10);
-    if (size == 0) {
-      close();
-      _loop.stop();
-      return;
-    }
-    size = write(1, buf, size);
-  }
-  void on_write() {
-  }
-  void on_error() {
-  }
-};
+void on_signal(ftev::SignalWatcher<int> &watcher, int _) {
+  (void)_;
+  (void)watcher;
+  std::cout << "Signal received!" << std::endl;
+}
 
-class MySignalWatcher : public ftev::EventLoop::BaseSignalWatcher {
-public:
-  MySignalWatcher(ftev::EventLoop &loop)
-	  : ftev::EventLoop::BaseSignalWatcher(loop) {
-  }
-  ~MySignalWatcher() {
-  }
-  void on_signal() {
-	std::cout << "Signal received!" << std::endl;
-  }
-};
-
-class MyProcessWatcher : public ftev::EventLoop::BaseProcessWatcher {
-public:
-  MyProcessWatcher(ftev::EventLoop &loop)
-	  : ftev::EventLoop::BaseProcessWatcher(loop) {
-  }
-  ~MyProcessWatcher() {
-  }
-  void on_exit(int status) {
-	std::cout << "Process exited! " << status << std::endl;
-  }
-};
+void on_exit(ftev::ProcessWatcher<int> &watcher, int status, int _) {
+  (void)_;
+  (void)watcher;
+  std::cout << "Process exited! " << status << std::endl;
+}
 
 #include <unistd.h>
 
@@ -80,16 +43,17 @@ int main() {
   pid_t pid = fork();
   if (pid == 0) {
     char const *const argv[] = {"python", "-m", "http.server", NULL};
-	execve("/bin/python", const_cast<char* const*>(argv), environ);
+    execve("/bin/python", const_cast<char *const *>(argv), environ);
   }
   ftev::EventLoop &loop = ftev::EventLoop::default_loop;
-  MyTimerWatcher timer_watcher(loop);
-  MyIOWatcher io_watcher(loop, "./pipe");
-  MySignalWatcher signal_watcher(loop);
-  MyProcessWatcher process_watcher(loop);
 
-  timer_watcher.start(100);
-  io_watcher.start(ftpp::BaseSelector::READ);
+  ftev::TimerWatcher<int> timer_watcher(loop, on_timeout, 0);
+  ftev::IOWatcher<int> io_watcher(loop, on_read, NULL, NULL, 0);
+  ftev::SignalWatcher<int> signal_watcher(loop, on_signal, 0);
+  ftev::ProcessWatcher<int> process_watcher(loop, on_exit, 0);
+
+  timer_watcher.start(1000);
+  io_watcher.start(open("./pipe", O_RDONLY), ftpp::BaseSelector::READ);
   signal_watcher.start(SIGINT);
   process_watcher.start(pid);
   loop.run();
