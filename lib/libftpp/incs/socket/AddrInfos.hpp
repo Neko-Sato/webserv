b003205a70049f8abee30aca32c7d98f5a2506e9
Neko-Sato <hisatosi35350@gmail.com>
2024-12-06 05:21:56 +0900

    update

---
+++ lib/libftpp/incs/socket/AddrInfos.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AddrInfos.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 03:43:17 by hshimizu          #+#    #+#             */
+/*   Updated: 2024/12/06 05:16:11 by hshimizu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <netdb.h>
+
+namespace ftpp {
+
+class AddrInfos {
+private:
+  addrinfo *_addrinfo;
+
+  class iterator {
+  private:
+    addrinfo *_current;
+
+  public:
+    iterator(addrinfo const *info = NULL);
+    iterator(iterator const &rhs);
+    ~iterator();
+
+    iterator &operator=(iterator const &rhs);
+    iterator &operator++();
+    iterator operator++(int);
+    bool operator==(iterator const &rhs) const;
+    addrinfo const &operator*() const;
+    addrinfo const *operator->() const;
+  };
+
+public:
+  struct Hints : private addrinfo {
+  public:
+    Hints();
+    Hints(int family, int socktype, int protocol, int flags);
+    Hints(Hints const &rhs);
+    ~Hints();
+    Hints &operator=(Hints const &rhs);
+
+    using addrinfo::ai_family;
+    using addrinfo::ai_flags;
+    using addrinfo::ai_protocol;
+    using addrinfo::ai_socktype;
+
+    addrinfo const *get_addrinfo() const;
+  };
+
+  AddrInfos(char const *name, char const *service);
+  AddrInfos(char const *name, char const *service, Hints const &hints);
+  ~AddrInfos();
+
+  iterator begin() const;
+  iterator end() const;
+};
+
+} // namespace ftpp
---
+++ lib/libftpp/srcs/socket/AddrInfos.cpp
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AddrInfos.cpp                                       :+:      :+:    :+: */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 03:43:17 by hshimizu          #+#    #+#             */
+/*   Updated: 2024/12/06 04:30:07 by hshimizu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <exceptions/OSError.hpp>
+#include <socket/AddrInfos.hpp>
+
+#include <cstring>
+
+namespace ftpp {
+
+AddrInfos::iterator::iterator(addrinfo const *info = NULL) : _current(info) {};
+AddrInfos::iterator::iterator(iterator const &rhs) : _current(rhs._current) {};
+
+AddrInfos::iterator::~iterator() {};
+
+iterator &AddrInfos::iterator::operator=(iterator const &rhs) {
+  if (this != &rhs) {
+    _current = rhs._current;
+  }
+  return *this;
+};
+
+iterator &AddrInfos::iterator::operator++() {
+  _current = _current->ai_next;
+  return *this;
+};
+
+iterator AddrInfos::iterator::operator++(int) {
+  iterator tmp(*this);
+  _current = _current->ai_next;
+  return tmp;
+};
+
+bool AddrInfos::iterator::operator==(iterator const &rhs) const {
+  return _current == rhs._current;
+};
+
+addrinfo const &AddrInfos::iterator::operator*() const {
+  return *_current;
+};
+
+addrinfo const *AddrInfos::iterator::operator->() const {
+  return _current;
+};
+
+AddrInfos::Hints::Hints() {
+  memset(static_cast<addrinfo *>(this), 0, sizeof(addrinfo));
+};
+
+AddrInfos::Hints::Hints(int family, int socktype, int protocol, int flags) {
+  memset(static_cast<addrinfo *>(this), 0, sizeof(addrinfo));
+  ai_family = family;
+  ai_socktype = socktype;
+  ai_protocol = protocol;
+  ai_flags = flags;
+};
+
+AddrInfos::Hints::Hints(Hints const &rhs) {
+  *static_cast<addrinfo *>(this) = static_cast<addrinfo const &>(rhs)
+};
+
+AddrInfos::Hints::~Hints() {};
+
+AddrInfos::Hints &AddrInfos::Hints::operator=(Hints const &rhs) {
+  if (this != &rhs) {
+    *static_cast<addrinfo *>(this) = static_cast<addrinfo const &>(rhs);
+  }
+  return *this;
+};
+
+addrinfo const *AddrInfos::Hints::get_addrinfo() const {
+  return static_cast<addrinfo const *>(this);
+};
+
+AddrInfos::AddrInfos(char const *name, char const *service) {
+  if (__glibc_unlikely(::getaddrinfo(name, service, NULL, &_addrinfo) == -1))
+    throw OSError(errno, "getaddrinfo");
+};
+
+AddrInfos::AddrInfos(char const *name, char const *service,
+                     Hints const &hints) {
+  if (__glibc_unlikely(::getaddrinfo(name, service, hints->get_addrinfo(),
+                                     &_addrinfo) == -1))
+    throw OSError(errno, "getaddrinfo");
+};
+
+AddrInfos::~AddrInfos() {
+  freeaddrinfo(_addrinfo);
+};
+
+iterator AddrInfos::begin() const {
+  return iterator(_addrinfo);
+};
+
+iterator AddrInfos::end() const {
+  return iterator(NULL);
+};
+
+} // namespace ftpp
