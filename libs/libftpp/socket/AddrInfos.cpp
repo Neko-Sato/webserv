6c20c3abd4df6b40f6320e1a3b61da5f73f4a727
Neko-Sato <hisatosi35350@gmail.com>
2025-01-05 21:55:26 +0900

    update

--- libs/libftpp/socket/AddrInfos.cpp
+++ libs/libftpp/socket/AddrInfos.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 04:39:48 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/01/03 21:33:00 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/01/05 21:52:58 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,48 +18,51 @@
 
 namespace ftpp {
 
-AddrInfos::iterator::iterator(addrinfo const *info) : _current(info) {};
-AddrInfos::iterator::iterator(iterator const &rhs) : _current(rhs._current) {};
+AddrInfos::iterator::iterator(addrinfo const *info) : _current(info) {
+}
+AddrInfos::iterator::iterator(iterator const &rhs) : _current(rhs._current) {
+}
 
-AddrInfos::iterator::~iterator() {};
+AddrInfos::iterator::~iterator() {
+}
 
 AddrInfos::iterator &AddrInfos::iterator::operator=(iterator const &rhs) {
   if (this != &rhs) {
     _current = rhs._current;
   }
   return *this;
-};
+}
 
 AddrInfos::iterator &AddrInfos::iterator::operator++() {
   _current = _current->ai_next;
   return *this;
-};
+}
 
 AddrInfos::iterator AddrInfos::iterator::operator++(int) {
   iterator tmp(*this);
   _current = _current->ai_next;
   return tmp;
-};
+}
 
 bool AddrInfos::iterator::operator==(iterator const &rhs) const {
   return _current == rhs._current;
-};
+}
 
 bool AddrInfos::iterator::operator!=(iterator const &rhs) const {
   return _current != rhs._current;
-};
+}
 
 addrinfo const &AddrInfos::iterator::operator*() const {
   return *_current;
-};
+}
 
 addrinfo const *AddrInfos::iterator::operator->() const {
   return _current;
-};
+}
 
 AddrInfos::Hints::Hints() {
   memset(static_cast<addrinfo *>(this), 0, sizeof(addrinfo));
-};
+}
 
 AddrInfos::Hints::Hints(int family, int socktype, int protocol, int flags) {
   memset(static_cast<addrinfo *>(this), 0, sizeof(addrinfo));
@@ -67,33 +70,36 @@ AddrInfos::Hints::Hints(int family, int socktype, int protocol, int flags) {
   ai_socktype = socktype;
   ai_protocol = protocol;
   ai_flags = flags;
-};
+}
 
 AddrInfos::Hints::Hints(Hints const &rhs) {
   *static_cast<addrinfo *>(this) = static_cast<addrinfo const &>(rhs);
-};
+}
 
-AddrInfos::Hints::~Hints() {};
+AddrInfos::Hints::~Hints() {
+}
 
 AddrInfos::Hints &AddrInfos::Hints::operator=(Hints const &rhs) {
   if (this != &rhs) {
     *static_cast<addrinfo *>(this) = static_cast<addrinfo const &>(rhs);
   }
   return *this;
-};
+}
 
 addrinfo const *AddrInfos::Hints::get_addrinfo() const {
   return static_cast<addrinfo const *>(this);
-};
+}
 
 AddrInfos::GAIError::GAIError(int __errno)
-    : _errno(__errno),
-      _s(std::string("getaddrinfo: ") + gai_strerror(_errno)) {};
+    : _errno(__errno), _s(std::string("getaddrinfo: ") + gai_strerror(_errno)) {
+}
 
 AddrInfos::GAIError::GAIError(GAIError const &rhs)
-    : _errno(rhs._errno), _s(rhs._s) {};
+    : _errno(rhs._errno), _s(rhs._s) {
+}
 
-AddrInfos::GAIError::~GAIError() throw() {};
+AddrInfos::GAIError::~GAIError() throw() {
+}
 
 AddrInfos::GAIError &AddrInfos::GAIError::operator=(GAIError const &rhs) {
   if (this != &rhs) {
@@ -101,15 +107,15 @@ AddrInfos::GAIError &AddrInfos::GAIError::operator=(GAIError const &rhs) {
     _s = rhs._s;
   }
   return *this;
-};
+}
 
 int AddrInfos::GAIError::get_errno() const {
   return _errno;
-};
+}
 
 char const *AddrInfos::GAIError::what() const throw() {
   return _s.c_str();
-};
+}
 
 addrinfo *AddrInfos::_getaddrinfo(char const *name, char const *service,
                                   addrinfo const *hints) {
@@ -122,23 +128,23 @@ addrinfo *AddrInfos::_getaddrinfo(char const *name, char const *service,
 
 AddrInfos::AddrInfos(char const *name, char const *service) {
   _addrinfo = _getaddrinfo(name, service);
-};
+}
 
 AddrInfos::AddrInfos(char const *name, char const *service,
                      Hints const &hints) {
   _addrinfo = _getaddrinfo(name, service, hints.get_addrinfo());
-};
+}
 
 AddrInfos::~AddrInfos() {
   freeaddrinfo(_addrinfo);
-};
+}
 
 AddrInfos::iterator AddrInfos::begin() const {
   return iterator(_addrinfo);
-};
+}
 
 AddrInfos::iterator AddrInfos::end() const {
   return iterator(NULL);
-};
+}
 
 } // namespace ftpp
--- libs/libftpp/unicode/UnicodeError.cpp
+++ libs/libftpp/unicode/UnicodeError.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 22:04:58 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/12/25 04:36:43 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/01/05 21:51:59 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 namespace ftpp {
 
 UnicodeError::UnicodeError(std::string const &s)
-    : _s(std::string("UnicodeError: ") + s) {};
+    : _s(std::string("UnicodeError: ") + s) {
+}
 
-UnicodeError::UnicodeError(UnicodeError const &rhs) : _s(rhs._s) {};
+UnicodeError::UnicodeError(UnicodeError const &rhs) : _s(rhs._s) {
+}
 
-UnicodeError::~UnicodeError() throw() {};
+UnicodeError::~UnicodeError() throw() {
+}
 
 UnicodeError &UnicodeError::operator=(UnicodeError const &rhs) {
   if (this != &rhs) {
