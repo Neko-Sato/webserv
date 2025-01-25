c6152345f34a5f714c6f2d14acb6c2de483c5823
Neko-Sato <hisatosi35350@gmail.com>
2025-01-25 10:55:46 +0900

    update

--- srcs/utility.cpp
+++ srcs/utility.cpp
@@ -6,31 +6,36 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 21:52:35 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/01/04 22:43:03 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/01/25 10:55:27 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_string.hpp>
 
-#include <macros.hpp>
+#include <cmath>
+#include <limits>
 #include <stdexcept>
 
 std::size_t parseSize(std::string const &str) {
-  std::size_t pos;
-  std::size_t result(ftpp::stoul(str, &pos));
+  if (str.empty() || !isdigit(str[0]))
+    throw std::invalid_argument("invalid size");
+  double size;
+  std::size_t unit;
   {
-    std::size_t unit = 1;
+    std::size_t pos;
+    size = ftpp::stod(str, &pos);
     std::string tmp(str, pos, std::string::npos);
-    if (tmp == "k" || tmp == "K")
+    if (tmp.empty())
+      unit = 1;
+    else if (tmp == "k" || tmp == "K")
       unit = 1ul << 10;
     else if (tmp == "m" || tmp == "M")
       unit = 1ul << 20;
     else if (tmp == "g" || tmp == "G")
       unit = 1ul << 30;
-    else if (!tmp.empty())
-      throw std::runtime_error("invalid size unit");
-    if (mul_overflow(result, unit, &result))
-      throw std::runtime_error("size overflow");
+    throw std::runtime_error("invalid size unit");
   }
-  return result;
+  if (size > std::numeric_limits<std::size_t>::max() / unit)
+    throw std::overflow_error("size overflow");
+  return static_cast<std::size_t>(size * unit);
 }
