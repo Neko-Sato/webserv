a0ccf3f8912510b22ef3709b0b6d70b079f9dbda
hshimizu <hshimizu@student.42tokyo.jp>
2025-04-24 03:36:06 +0900

    update

--- srcs/tasks/RedirectTask.cpp
+++ srcs/tasks/RedirectTask.cpp
@@ -6,11 +6,12 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 17:56:36 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/04/24 03:03:34 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/04/24 03:34:28 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tasks/RedirectTask.hpp"
+#include "Cycle.hpp"
 #include "constants.hpp"
 #include "structs/Response.hpp"
 
@@ -19,7 +20,6 @@
 RedirectTask::RedirectTask(Connection::Cycle &cycle,
                            LocationRedirect const &location)
     : Task(cycle), _location(location) {
-  (void)_location;
 }
 
 RedirectTask::~RedirectTask() {
@@ -29,16 +29,11 @@ void RedirectTask::onData(std::vector<char> const &) {
 }
 
 void RedirectTask::onEof() {
-  //   std::string response;
-  //   Response tmp;
-  //   tmp.version = "HTTP/1.1";
-  //   tmp.status = _location.getCode();
-  //   HttpStatusMap::const_iterator it = httpStatusMap.find(tmp.status);
-  //   tmp.reason = it != httpStatusMap.end() ? it->second : "Unknown";
-  //   tmp.headers["Content-Type"].push_back("text/html");
-  //   tmp.headers["Location"].push_back(_location.getRedirect());
-  //   composeResponse(response, tmp);
-  //   transport.write(response.c_str(), response.size());
+  Response::Headers headers;
+  headers["Location"].push_back(_location.getRedirect());
+  headers["Content-Type"].push_back("text/html");
+  cycle.send(_location.getCode(), headers);
+  cycle.send(NULL, 0, false);
 }
 
 void RedirectTask::onCancel() {
