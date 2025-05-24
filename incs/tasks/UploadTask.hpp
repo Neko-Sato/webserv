cb79145109319934f76c0e2ef69b7fb6ec2782d5
hshimizu <hshimizu@student.42tokyo.jp>
2025-05-25 05:11:18 +0900

    update

--- incs/tasks/UploadTask.hpp
+++ incs/tasks/UploadTask.hpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 18:00:43 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/05/13 20:24:41 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/05/25 04:52:29 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 #include "locations/LocationUpload.hpp"
 #include "tasks/Task.hpp"
 
+#include <deque>
+
 class UploadTask : public Task {
 private:
+  enum State { INIT, DATA, EOF };
+
   LocationUpload const &_location;
   int _status;
+  std::deque<char> _buffer;
+  std::string _boundary;
 
 public:
   UploadTask(Connection::Cycle &cycle, LocationUpload const &location);
--- srcs/tasks/UploadTask.cpp
+++ srcs/tasks/UploadTask.cpp
@@ -6,30 +6,70 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 18:00:43 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/04/24 22:38:08 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/05/25 04:55:36 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tasks/UploadTask.hpp"
 #include "Cycle.hpp"
+#include "HttpException.hpp"
+#include "constants.hpp"
+
+#include <ftpp/algorithm.hpp>
+#include <ftpp/string/string.hpp>
+
+#include <iterator>
+#include <set>
 
 UploadTask::UploadTask(Connection::Cycle &cycle, LocationUpload const &location)
-    : Task(cycle), _location(location) {
-  (void)_location;
+    : Task(cycle), _location(location), _status(-1) {
+  try {
+    Request const &request = cycle.getRequest();
+    if (request.method != "POST")
+      throw HttpException(405);
+    Headers::const_iterator it;
+    if ((it = request.headers.find("content-type")) == request.headers.end())
+      throw HttpException(400);
+    std::multiset<std::string> params;
+    ftpp::strsplit(it->second[0], ";", std::inserter(params, params.end()));
+    {
+      std::multiset<std::string> tmp;
+      std::transform(params.begin(), params.end(),
+                     std::inserter(tmp, tmp.end()), ftpp::strtrim);
+      params.swap(tmp);
+    }
+    if (params.find("multipart/form-data") == params.end())
+      throw HttpException(400);
+    std::multiset<std::string>::iterator jt;
+    if ((jt = params.lower_bound("boundary=")) == params.end())
+      throw HttpException(400);
+    if (jt->compare(0, 9, "boundary="))
+      throw HttpException(400);
+    _boundary = jt->substr(9);
+  } catch (HttpException &e) {
+    _status = e.getStatus();
+  } catch (...) {
+    _status = 500;
+  }
 }
 
 UploadTask::~UploadTask() {
 }
 
-void UploadTask::onData(std::vector<char> const &) {
+void UploadTask::onData(std::vector<char> const &data) {
+  if (_status != -1)
+    return;
+  std::cout << _boundary << std::endl;
+  _buffer.insert(_buffer.end(), data.begin(), data.end());
 }
 
 void UploadTask::onEof() {
-  Response::Headers headers;
-  headers["Content-Type"].push_back("text/plain");
-  cycle.send(200, headers);
-  cycle.send("UploadTask", 10, false);
-}
-
-void UploadTask::onCancel() {
+  if (_status != -1) {
+    cycle.sendErrorPage(_status);
+    return;
+  }
+  Headers headers;
+  headers["Location"].push_back(_location.getRedirect());
+  cycle.send(303, headers);
+  cycle.send(NULL, 0, false);
 }
