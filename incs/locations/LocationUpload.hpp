12671bc7f125d0bfe0d218c9649d1e8531cfeae4
hshimizu <hshimizu@student.42tokyo.jp>
2025-05-25 04:46:26 +0900

    update

--- incs/locations/LocationUpload.hpp
+++ incs/locations/LocationUpload.hpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 18:38:02 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/04/24 03:25:13 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/05/24 05:31:23 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@
 class LocationUpload : public Location::Detail {
 private:
   std::string _store;
+  std::string _redirect;
 
   void _takeStore(ftjson::Object const &location);
+  void _takeRedirect(ftjson::Object const &location);
 
 public:
   LocationUpload();
@@ -31,6 +33,7 @@ public:
   LocationUpload *clone() const;
 
   std::string const &getStore() const;
+  std::string const &getRedirect() const;
 
   Task *createTask(Connection::Cycle &cycle) const;
 };
