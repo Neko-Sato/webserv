53aba9e5dddc81fee34405b7dd2492094f5ea147
Adrian gutierrez <19gutierreza@gmail.com>
2025-06-20 02:09:04 +0900

    updated UploadTask.cpp

--- srcs/tasks/UploadTask.cpp
+++ srcs/tasks/UploadTask.cpp
@@ -6,7 +6,7 @@
 /*   By: adrgutie <adrgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 18:00:43 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/06/11 11:18:25 by adrgutie         ###   ########.fr       */
+/*   Updated: 2025/06/20 02:08:36 by adrgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,10 @@
 #include <iterator>
 #include <set>
 #include <ctime>
+#include <ftpp/html/html.hpp>
+#include <ftpp/urllib/urlquote.hpp>
+#include <vector>
+#include <cstdlib>
 
 UploadTask::UploadTask(Connection::Cycle &cycle, LocationUpload const &location)
     : Task(cycle), _location(location), _status(-1) {
@@ -66,13 +70,13 @@ void UploadTask::onData(std::vector<char> const &data) {
 }
 
 static bool isValidFilename(const std::string& filename) {
-  if (filename.length() > 255 || filename.length() == 0)
+  if (filename.length() > 255)
     return false;
   if (filename.find("..") != std::string::npos)
     return false;
   if (filename == ".")
     return false;
-  std::string disallowedChars = "\\/:*?\"<>|";
+  static const std::string disallowedChars = "\\/:*?\"<>|";
   for (size_t i = 0; i < disallowedChars.length(); i++)
   {
     if (filename.find(disallowedChars[i]) != std::string::npos)
@@ -81,67 +85,99 @@ static bool isValidFilename(const std::string& filename) {
   return true;
 }
 
+static std::string randomString(size_t length)
+{
+  static const std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYXZ";
+  std::string rez;
+  for (size_t i = 0; i < length; i++)
+    rez += alphabet[rand() % alphabet.size()];
+  return rez;
+}
+
 void UploadTask::onEof() {
   if (_status != -1) {
     cycle.sendErrorPage(_status);
     return;
   }
+  bool requestStarted = false;
   try
   {
-    std::string body(_buffer.begin(), _buffer.end());
+    std::vector<char> body(_buffer.begin(), _buffer.end());
     std::string delim = "--" + _boundary;
     std::string delimend = delim + "--";
     size_t pos = 0;
-    while ((pos = body.find(delim, pos)) != std::string::npos )
+    while (true)
     {
-      pos += delim.length();
-      if (body.compare(pos, 2, CRLF) == 0)
-        pos += 2;
-      size_t next = body.find(delim, pos);
-      if (next == std::string::npos)
+      std::vector<char>::iterator it = std::search(body.begin() + pos, \
+                                body.end(), delim.begin(), delim.end());
+      if (it == body.end())
         break ;
-      std::string part = body.substr(pos, next - pos);
-      pos = next;
-      size_t header_end = part.find(DOUBLE_CRLF);
-      if (header_end == std::string::npos)
+      pos = it - body.begin();
+      pos += delim.length();
+      if (pos + 2 <= body.size() && body[pos] == '\r' && body[pos + 1] == '\n')
+        pos += 2;
+      std::vector<char>::iterator next_it = std::search(body.begin() + pos, \
+                                  body.end(), delim.begin(), delim.end());
+      if (next_it == body.end())
+        break ;
+      std::vector<char> part(body.begin() + pos, next_it);
+      std::vector<char>::iterator header_it = std::search(
+              part.begin(), part.end(), DOUBLE_CRLF.begin(), DOUBLE_CRLF.end());
+      if (header_it == part.end())
         continue ;
-      std::string headers_str = part.substr(0, header_end);
-      std::string content = part.substr(header_end + 4);
-      size_t name_pos = headers_str.find("filename=\"");
-      if (name_pos == std::string::npos)
-        continue ;
-      size_t name_end = headers_str.find("\"", name_pos + 10);
-      std::string filename = headers_str.substr(name_pos + 10, name_end - name_pos - 10);
-
+      std::vector<char> headers_vec(part.begin(), header_it);
+      std::vector<char> content_vec(header_it + 4, part.end());
+      static const std::string fileNameEquals = "filename=\"";
+      static const std::string quote = "\"";
+      std::vector<char>::iterator name_it = std::search( \
+          headers_vec.begin(), headers_vec.end(), \
+          fileNameEquals.begin(), fileNameEquals.end());
+      if (name_it == headers_vec.end())
+      {
+        cycle.sendErrorPage(400);
+        return;
+      }
+      std::vector<char>::iterator name_end_it = std::search( \
+          name_it + 10, headers_vec.end(), \
+          quote.begin(), quote.end());
+      std::string filename(name_it + 10, name_end_it);
+      //urlunquote filename
+      filename = ftpp::urlunquote(filename);
       //check filename
       if (isValidFilename(filename) == false)
+      {
         cycle.sendErrorPage(400);
+        return ;
+      }
       //get unique filename
       std::time_t now = std::time(0);
-      std::ostringstream currenttime;
-      currenttime << now;
-      std::string filename_with_numbers = currenttime.str() + "_" + filename;
+      std::string filename_with_numbers = ftpp::to_string(now) + "_" + \
+          randomString(5) + "_" + filename;
       //put content in file
       std::string path = _location.getStore() + "/" + filename_with_numbers;
       std::ofstream ofs(path.c_str(), std::ios::binary);
-
       //checking if new file openned
       if (!ofs.is_open() || ofs.fail()) {
       //std::cerr << "Error: Could not open file for writing at path: " << path << std::endl;
-      cycle.sendErrorPage(500); 
+        cycle.sendErrorPage(500); 
+        return ;
       }
       //writing to file
-      ofs.write(content.c_str(), content.size());
+      ofs.write(content_vec.data(), content_vec.size());
       ofs.close();
     }
     //redirection stuff
     Headers headers;
     headers["Location"].push_back(_location.getRedirect());
     cycle.send(303, headers);
+    requestStarted = true;
     cycle.send(NULL, 0, false);
   }
   catch(...)
   {
-    cycle.sendErrorPage(500);
+    //std::cerr << e.what() << std::endl;
+    if (requestStarted == false)
+      cycle.sendErrorPage(500);
+    return ;
   }
 }
