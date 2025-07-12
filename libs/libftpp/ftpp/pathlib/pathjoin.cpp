674f8e7abe05e14c515fb106c8aae01f9f8f4603
Uru Aki <uruaki@tsuki.local>
2025-07-12 20:04:02 +0900

    update

--- libs/libftpp/ftpp/pathlib/pathjoin.cpp
+++ libs/libftpp/ftpp/pathlib/pathjoin.cpp
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pathlib.hpp                                        :+:      :+:    :+:   */
+/*   pathjoin.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: uakizuki <uakizuki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/27 02:50:21 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/07/12 20:00:15 by uakizuki         ###   ########.fr       */
+/*   Created: 2025/07/12 19:58:08 by uakizuki          #+#    #+#             */
+/*   Updated: 2025/07/12 20:02:38 by uakizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-
-#include <string>
+#include <ftpp/pathlib/pathlib.hpp>
 
 namespace ftpp {
 
-std::string pathjoin(std::string const &path1, std::string const &path2);
-std::string normpath(std::string const &path);
-
-bool isAbsolutePath(std::string const &path);
+std::string pathjoin(std::string const &path1, std::string const &path2) {
+  std::string tmp(path1);
+  if (!tmp.empty() && *tmp.cbegin() == '/')
+    tmp += '/';
+  tmp += path2;
+  return normpath(tmp);
+}
 
 } // namespace ftpp
